460a742a48d277b8170dd7a73d8241e88a9faaaf
umesh p <ussp29@gmail.com>
2023-06-02 09:27:10 +0530

    done dana dananadan

--- ALLINone1/src/javaPrograms/REVERSEString1.java
+++ ALLINone1/src/javaPrograms/REVERSEString1.java
@@ -5,6 +5,8 @@ public class REVERSEString1 {
 
 	public static void main(String[] args) {
 		System.out.println("one more program");
+		System.out.println("fully description");
+		System.out.println("donn dana dann");
 		
 
 	}
